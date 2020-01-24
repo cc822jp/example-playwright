f8a47386b18003fbaf5ba533c5d8685290ef6a06
Tatsuya Sasaki <sasaki@crystalcode.jp>
2020-01-25 00:13:07 +0900

    update: run all browser

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # example-playwright
 
+## 使い方
 
-
+```
+git clone git@github.com:cc822jp/example-playwright.git
+cd example-playwright
+npm i
+node index.js
+```
--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const pw = require('playwright');
 
-(async () => {
-  const browser = await pw.webkit.launch(); // or 'chromium', 'firefox'
-  const context = await browser.newContext();
+/**
+ * get screenshot
+ * @param {string} engine
+ * @returns {Promise<void>}
+ */
+const screenshot = async (engine) => {
+  const browser = await pw[engine].launch();
+  const context = await browser.newContext({
+    viewport: {
+      'width': 1024,
+      'height': 768,
+      'deviceScaleFactor': 1,
+      'isMobile': true
+    }
+  });
   const page = await context.newPage();
 
-  await page.goto('https://www.google.com/');
-  await page.screenshot({path: 'example.png'});
+  await page.goto('https://www.yahoo.co.jp', {waitUntil: 'load'});
+  await page.screenshot({path: `example_${engine}.png`} );
 
   await browser.close();
+};
+
+(async () => {
+  // Safariでスクショ
+  await screenshot('webkit');
+  console.log("safariおわりました");
+
+  // Chromeでスクショ
+  await screenshot('chromium');
+  console.log("chromeおわりました");
+
+  // Firefoxでスクショ
+  await screenshot('firefox');
+  console.log("firefoxおわりました");
 })();
